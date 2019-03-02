4b00af26377baf585b48a5e472d600c1a6d4e344
Ajay Mandrawal <mac@Ajay-Mandrawal.local>
2019-03-02 15:28:02 +0530

    second

--- mylibrary2/src/main/java/com/example/mylibrary2/ToastActivity.java
+++ mylibrary2/src/main/java/com/example/mylibrary2/ToastActivity.java
@@ -1,13 +1,33 @@
 package com.example.mylibrary2;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class ToastActivity {
+public class ToastActivity extends AppCompatActivity {
 
     public static void s(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
     }
+
+    public static ToastActivity getInstance(){
+        ToastActivity toastActivity = new ToastActivity();
+        return toastActivity;
+    }
+
+
+
+    public  void startActivityChat (Context context){
+
+        Intent i= new Intent(ToastActivity.this ,ChatActivity.class);
+        ((Activity)context). startActivity(i);
+
+    }
+
+
+
 }
